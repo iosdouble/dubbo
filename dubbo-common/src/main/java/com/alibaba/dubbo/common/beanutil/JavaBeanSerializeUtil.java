4b12509b6b1ed2692d143507506bb4a86b877b3a
nihui <18202504057@163.com>
2019-05-08 17:36:49 +0800

    格式修订

--- dubbo-common/src/main/java/com/alibaba/dubbo/common/beanutil/JavaBeanSerializeUtil.java
+++ dubbo-common/src/main/java/com/alibaba/dubbo/common/beanutil/JavaBeanSerializeUtil.java
@@ -47,7 +47,7 @@ public final class JavaBeanSerializeUtil {
             return null;
         }
         if (obj instanceof JavaBeanDescriptor) {
-            return (JavaBeanDescriptor)obj;
+            return (JavaBeanDescriptor) obj;
         }
         IdentityHashMap<Object, JavaBeanDescriptor> cache = new IdentityHashMap<Object, JavaBeanDescriptor>();
         JavaBeanDescriptor result = createDescriptorIfAbsent(obj, accessor, cache);
@@ -76,7 +76,7 @@ public final class JavaBeanSerializeUtil {
         if (cache.containsKey(obj)) {
             return cache.get(obj);
         } else if (obj instanceof JavaBeanDescriptor) {
-            return (JavaBeanDescriptor)obj;
+            return (JavaBeanDescriptor) obj;
         } else {
             JavaBeanDescriptor result = createDescriptorForSerialize(obj.getClass());
             cache.put(obj, result);
@@ -167,8 +167,8 @@ public final class JavaBeanSerializeUtil {
 
     public static Object deserialize(JavaBeanDescriptor beanDescriptor) {
         Object result = deserialize(
-            beanDescriptor,
-            Thread.currentThread().getContextClassLoader());
+                beanDescriptor,
+                Thread.currentThread().getContextClassLoader());
         return result;
     }
 
@@ -277,7 +277,7 @@ public final class JavaBeanSerializeUtil {
         } catch (NoSuchMethodException e) {
             for (Method m : cls.getMethods()) {
                 if (ReflectUtils.isBeanPropertyWriteMethod(m)
-                    && m.getName().equals(name)) {
+                        && m.getName().equals(name)) {
                     method = m;
                 }
             }
@@ -419,9 +419,7 @@ public final class JavaBeanSerializeUtil {
      * 把 Class.forName 的返回值转换为 Class.
      *
      * @param name Class.getName()
-     *
      * @return Class
-     *
      * @throws ClassNotFoundException Class.forName
      */
     public static Class<?> name2Class(ClassLoader loader, String name) throws ClassNotFoundException {
@@ -453,8 +451,8 @@ public final class JavaBeanSerializeUtil {
 
     private static boolean isReferenceType(String type) {
         return type != null
-            && type.startsWith(REFERENCE_TYPE_PREFIX)
-            && type.endsWith(REFERENCE_TYPE_SUFFIX);
+                && type.startsWith(REFERENCE_TYPE_PREFIX)
+                && type.endsWith(REFERENCE_TYPE_SUFFIX);
     }
 
     private static Method getEnumValueOfMethod(Class cl) throws NoSuchMethodException {
